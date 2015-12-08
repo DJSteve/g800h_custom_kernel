edfca6dd0378fb2ed30516ec942a6372393ced21
Stephen Hyde <Steve1030uk@gmail.com>
2015-12-08 16:13:55 +0000

    tweaks and add 1.6ghz cpu overclock

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,7 +4,7 @@ export ARCH=arm
 export CROSS_COMPILE=/opt/toolchains/arm-eabi-4.7/bin/arm-eabi-
 mkdir output
 
-make -C $(pwd) O=output VARIANT_DEFCONFIG=msm8228-sec_atlantic3geur_defconfig msm8226-sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+make -C $(pwd) O=output kmini3g_custom_full_defconfig -j9
 make -C $(pwd) O=output -j9
 
 cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
