a90eacf1111753d3efbb9ac96b7ccae7a9389e08
Stephen Hyde <Steve1030uk@gmail.com>
2016-01-04 17:54:32 +0000

    Add Sound Control 3.2 and updates, continue work on charge/usb forced charge

--- arch/arm/mach-msm/fastchg.c
+++ arch/arm/mach-msm/fastchg.c
@@ -1,6 +1,8 @@
 /*
  * Author: Chad Froebel <chadfroebel@gmail.com>
  *
+ * Simple port to Nexus 4 : motley <motley.slate@gmail.com>
+ *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
  * may be copied, distributed, and modified under those terms.
@@ -12,13 +14,20 @@
  *
  */
 
+/*
+ * Possible values for "force_fast_charge" are :
+ *
+ *   0 - disabled (default)
+ *   1 - substitute AC to USB unconditional
+*/
+
 #include <linux/kobject.h>
 #include <linux/sysfs.h>
 #include <linux/fastchg.h>
 
 int force_fast_charge;
 
-/* sysfs interface */
+/* sysfs interface for "force_fast_charge" */
 static ssize_t force_fast_charge_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
 {
 return sprintf(buf, "%d\n", force_fast_charge);
@@ -26,40 +35,55 @@ return sprintf(buf, "%d\n", force_fast_charge);
 
 static ssize_t force_fast_charge_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)
 {
-sscanf(buf, "%du", &force_fast_charge);
+
+int new_force_fast_charge;
+
+sscanf(buf, "%du", &new_force_fast_charge);
+
+if (new_force_fast_charge >= FAST_CHARGE_DISABLED && new_force_fast_charge <= FAST_CHARGE_FORCE_AC) {
+	/* update only if valid value provided */
+	force_fast_charge = new_force_fast_charge;
+}
+
 return count;
 }
 
 static struct kobj_attribute force_fast_charge_attribute =
 __ATTR(force_fast_charge, 0666, force_fast_charge_show, force_fast_charge_store);
 
-static struct attribute *attrs[] = {
+static struct attribute *force_fast_charge_attrs[] = {
 &force_fast_charge_attribute.attr,
 NULL,
 };
 
-static struct attribute_group attr_group = {
-.attrs = attrs,
+static struct attribute_group force_fast_charge_attr_group = {
+.attrs = force_fast_charge_attrs,
 };
 
+/* Initialize fast charge sysfs folder */
 static struct kobject *force_fast_charge_kobj;
 
 int force_fast_charge_init(void)
 {
-	int retval;
+	int force_fast_charge_retval;
 
-	force_fast_charge = 0;
+	force_fast_charge = FAST_CHARGE_DISABLED; /* Forced fast charge disabled by default */
 
-        force_fast_charge_kobj = kobject_create_and_add("fast_charge", kernel_kobj);
-        if (!force_fast_charge_kobj) {
-                return -ENOMEM;
-        }
-        retval = sysfs_create_group(force_fast_charge_kobj, &attr_group);
-        if (retval)
-                kobject_put(force_fast_charge_kobj);
-        return retval;
+	force_fast_charge_kobj = kobject_create_and_add("fast_charge", kernel_kobj);
+	if (!force_fast_charge_kobj) {
+			return -ENOMEM;
+	}
+
+	force_fast_charge_retval = sysfs_create_group(force_fast_charge_kobj, &force_fast_charge_attr_group);
+
+	if (force_fast_charge_retval)
+		kobject_put(force_fast_charge_kobj);
+
+	if (force_fast_charge_retval)
+		kobject_put(force_fast_charge_kobj);
+
+	return (force_fast_charge_retval);
 }
-/* end sysfs interface */
 
 void force_fast_charge_exit(void)
 {
@@ -68,3 +92,4 @@ void force_fast_charge_exit(void)
 
 module_init(force_fast_charge_init);
 module_exit(force_fast_charge_exit);
+
